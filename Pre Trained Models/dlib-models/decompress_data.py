13784bc19067a26e982438f8dcae85df7586f604
Anubhav.Singh <Anubhav.Singh@bold.com>
2019-08-22 15:37:44 +0530

    initial commit

---
+++ Pre Trained Models/dlib-models/decompress_data.py
@@ -0,0 +1,14 @@
+import bz2
+import os
+dirpath = 'D:\Projects\Python\Models Downloaded\dlib-models'
+files = []
+for file in os.listdir(dirpath):
+    if file.endswith(".bz2"):
+        files.append(file)
+
+for filename in files:
+    filepath = os.path.join(dirpath, filename)
+    newfilepath = os.path.join(dirpath, filename.replace('.bz2', '') )
+    with open(newfilepath, 'wb') as new_file, bz2.BZ2File(filepath, 'rb') as file:
+        for data in iter(lambda : file.read(100 * 1024), b''):
+            new_file.write(data)
